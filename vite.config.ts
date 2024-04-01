5abfde81f99d25ffab824c34ddffec833298ce93
Deepak Pradhan <deepak.pradhan048@gmail.com>
2024-04-01 18:09:16 +0545

    feat: build implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/main.tsx",
+      name: "varun-ui-library",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // Make sure to externalize dependencies in your library build
+      // Externalize dependencies that shouldn't be bundled into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        globals: {},
+      },
+    },
+  },
   plugins: [react()],
-})
+});
